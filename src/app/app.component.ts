0b6ed4cffe8d46e2cea74258f2d7b2d50ea9372d
edwardcjk <jinkhangc@gmail.com>
2018-12-07 15:52:47 +0800

    PSPDFKIT version 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,6 +25,12 @@ export class MyApp {
       { title: 'List', component: ListPage }
     ];
 
+
+
+    PSPDFKitPlugin.setLicenseKey('r3nvXVWFWeMO2gOG-z3mSiQAqJdUso3RUEMX-qWaIRxxOu42sOMvb7Vuq8pb2ETA1HqxmJST2tpnb3_6-bGsk7zbog6Wzl16h_CGmfrg3qlpd1QiM4j3zPA_8oYhYETVaLuOyszbaVie_1Gs8IhRQr3-ZIEjS3yB4G1C-58HwpW8-fz1rGBPDXxHkueNnhCzpMhqUpelkRar0nE8tt5I-CplMbYS7vomLnlFwPpSxgYMIEZ7hICRH62urs8Jq5O9DrtE6zM1cjiVondHyyzC_7Qw2jwE2PKAAdRhLZFM7BQwQc3QHk4qQJQblXcpQt2XjLKE-Wll7ZpgJrnVnDaJoXVyYfePj3b2EaLy5UoigadsiDFscguv6PY_V0g2Uah-z3maRRqIxX7zjqcbP00wXIwbNCt1OCA3AmgtFm-p7sHsVHUCvwaHFmeMrXHUbuKf');
+    PSPDFKitPlugin.present('pdf/document.pdf', {});
+
+
   }
 
   initializeApp() {
